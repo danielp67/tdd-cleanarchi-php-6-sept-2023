f50baaa7c402fd9b847dbf8deae9917b2f59788d
Michaël AZERHAD <mazerhad@wealcomecompany.com>
2023-09-06 23:51:10 +0200

    use a beforeEach within unit test to ease readability

--- tests/Unit/UberTop/RideBookingContext/BusinessLogic/UseCases/RideBooking/BookRideTest.php
+++ tests/Unit/UberTop/RideBookingContext/BusinessLogic/UseCases/RideBooking/BookRideTest.php
@@ -6,27 +6,23 @@ use App\UberTop\RideBookingContext\BusinessLogic\Models\DeterministicDateProvide
 use App\UberTop\RideBookingContext\BusinessLogic\Models\Ride;
 use App\UberTop\RideBookingContext\BusinessLogic\UseCases\RideBooking\BookRide;
 
-/**
- * @param RideRepositoryStub $rideRepository
- * @param $arrival
- * @param $expectedPrice
- * @return void
- */
+beforeEach(function () {
+    $this->rideRepository = new RideRepositoryStub();
+    $this->tripScanner = new TripScannerStub();
+    $this->dateProvider = new DeterministicDateProvider();
+    $this->bookRide = new BookRide($this->rideRepository, $this->tripScanner, $this->dateProvider);
+    $this->dateProvider->setDateNow(new \DateTime('2022-03-04'));
+});
 
-it('can book a basic ride with some distance', function(
+it('can book a basic ride with some distance', function (
     $arrival, $distance, $expectedPrice
 ) {
     // ARRANGE
-    $rideRepository = new RideRepositoryStub();
-    $tripScanner = new TripScannerStub();
-    $dateProvider = new DeterministicDateProvider();
-    $dateProvider->setDateNow(new \DateTime('2022-03-04'));
-    $bookRide = new BookRide($rideRepository, $tripScanner, $dateProvider);
-    $tripScanner->setDistance($distance);
+    $this->tripScanner->setDistance($distance);
     // ACT
-    $bookRide->book("8 avenue Foch Paris", $arrival, false);
+    $this->bookRide->book("8 avenue Foch Paris", $arrival, false);
     // ASSERT
-    expectBookedRides($rideRepository, $arrival, $expectedPrice);
+    expectBookedRides($this->rideRepository, $arrival, $expectedPrice);
 })->with([
     "distance of 0km" => ["8 avenue Foch Paris", 0, 10],
     "distance of 1km" => ["199 avenue Foch Paris", 1, 10.5],
@@ -34,39 +30,30 @@ it('can book a basic ride with some distance', function(
 ]);
 
 
-it('can book a basic ride with adapted price for uberX option', function(
+it('can book a basic ride with adapted price for uberX option', function (
     $wantsUberX, $arrival, $distance, $expectedPrice
 ) {
     // ARRANGE
-    $rideRepository = new RideRepositoryStub();
-    $tripScanner = new TripScannerStub();
-    $dateProvider = new DeterministicDateProvider();
-    $bookRide = new BookRide($rideRepository, $tripScanner, $dateProvider);
-    $dateProvider->setDateNow(new \DateTime('2022-03-04'));
-    $tripScanner->setDistance($distance);
+    $this->tripScanner->setDistance($distance);
     // ACT
-    $bookRide->book("8 avenue Foch Paris", $arrival, $wantsUberX);
+    $this->bookRide->book("8 avenue Foch Paris", $arrival, $wantsUberX);
     // ASSERT
-    expectBookedRides($rideRepository, $arrival, $expectedPrice);
+    expectBookedRides($this->rideRepository, $arrival, $expectedPrice);
 })->with([
     "wants UberX for 0km" => [true, "8 avenue Foch Paris", 0, 15],
     "wants UberX for 1km" => [true, "199 avenue Foch Paris", 1, 15.5],
 ]);
 
-it('should double the price because Christmas', function(
+it('should double the price because Christmas', function (
     $wantsUberX, $distance, $expectedPrice
 ) {
     // ARRANGE
-    $rideRepository = new RideRepositoryStub();
-    $tripScanner = new TripScannerStub();
-    $tripScanner->setDistance($distance);
-    $dateProvider = new DeterministicDateProvider();
-    $dateProvider->setDateNow(new DateTime('2023-12-25'));
-    $bookRide = new BookRide($rideRepository, $tripScanner, $dateProvider);
+    $this->dateProvider->setDateNow(new DateTime('2023-12-25'));
+    $this->tripScanner->setDistance($distance);
     // ACT
-    $bookRide->book("8 avenue Foch Paris", "8 avenue Foch Paris", $wantsUberX);
+    $this->bookRide->book("8 avenue Foch Paris", "8 avenue Foch Paris", $wantsUberX);
     // ASSERT
-    expectBookedRides($rideRepository, "8 avenue Foch Paris", $expectedPrice);
+    expectBookedRides($this->rideRepository, "8 avenue Foch Paris", $expectedPrice);
 })->with([
     "no distance ride on Christmas" => [false, 0, 20],
     "with distance ride on Christmas" => [false, 1, 21],
