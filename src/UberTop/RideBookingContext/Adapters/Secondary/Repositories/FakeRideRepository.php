b561e43e07b17236f016aa391b86671e55422479
Michaël AZERHAD <mazerhad@wealcomecompany.com>
2023-09-07 15:08:41 +0200

    implement byId with no code in FakeRideRepository

--- src/UberTop/RideBookingContext/Adapters/Secondary/Repositories/FakeRideRepository.php
+++ src/UberTop/RideBookingContext/Adapters/Secondary/Repositories/FakeRideRepository.php
@@ -4,6 +4,7 @@ namespace App\UberTop\RideBookingContext\Adapters\Secondary\Repositories;
 
 use App\UberTop\RideBookingContext\BusinessLogic\Models\Ride;
 use App\UberTop\RideBookingContext\BusinessLogic\SecondaryPorts\Repositories\RideRepository;
+use Ramsey\Uuid\UuidInterface;
 
 class FakeRideRepository implements RideRepository
 {
@@ -12,6 +13,11 @@ class FakeRideRepository implements RideRepository
     public function save(Ride $ride): void
     {
         $this->rides[] = $ride;
+    }
+
+    public function byId(UuidInterface $rideId)
+    {
+        // TODO: Implement byId() method.
     }
 
     public function rides(): array
